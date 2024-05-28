0e8e9f8721ae836fe539d5c20861791f8f34ae73
geonwule <geonwlee22@gmail.com>
2024-05-28 15:45:54 +0900

    ✨ feat: oauth setting and main setting

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello World!")
+from set_oauth import get_access_token, get_days_until_blackhole, get_user_info
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print('Go to the following link and get the code:\n', os.getenv("TARGET_URI"))
+while True:
+    code = input('Enter the code: ')
+    access_token, expires_in = get_access_token(code)
+    if (access_token == None):
+        print('Failed to get access token')
+        continue
+    break
+
+print('access_token = ', access_token, 'expires_in = ', expires_in)
+
+while True:
+    intra_id = input("Enter your intra ID: ")
+    if intra_id == 'exit':
+        break
+
+    try:
+        user_info = get_user_info(access_token, intra_id)
+        is_blackholed = False
+        for cursus_user in user_info['cursus_users']:
+            if cursus_user['blackholed_at']:
+                days_until_blackhole = get_days_until_blackhole(cursus_user['blackholed_at'])
+                is_blackholed = True
+                break
+        if is_blackholed:
+            print(f"{intra_id}'s blackhole in {days_until_blackhole} days")
+        else:
+            print(f"{intra_id} is not blackholed, maybe {intra_id} is member")
+
+    except:
+        print(intra_id, "is not found")
+        continue
---
+++ set_oauth.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from datetime import datetime, timezone
+
+def get_access_token(code):
+    auth_data = {
+        'grant_type': 'authorization_code',
+        'client_id': os.getenv('UID'),
+        'client_secret': os.getenv('SECRET'),
+        'code': code,
+        'redirect_uri': 'https://profile.intra.42.fr/'
+    }
+
+    response = requests.post('https://api.intra.42.fr/oauth/token', data=auth_data)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        return response_data['access_token'], response_data['expires_in']
+    else:
+        return None, None
+
+
+def get_days_until_blackhole(blackhole_date_str):
+    blackhole_date = datetime.fromisoformat(blackhole_date_str[:-1]) # 마지막 'Z' 제거
+    blackhole_date = blackhole_date.replace(tzinfo=timezone.utc) # Aware 객체로 변환
+    today = datetime.now(tz=timezone.utc) # 현재 시간을 UTC 시간대로 가져오기
+    delta = blackhole_date - today # 블랙홀까지 남은 시간 계산
+    return delta.days + 1
+
+def get_user_info(access_token, intra_id):
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.get(f'https://api.intra.42.fr/v2/users/{intra_id}', headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
